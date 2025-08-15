868f749455eff6fbc80c6a698189a61e344af255
eatdesignlove <eatdesignlove@gmail.com>
2025-08-15 16:31:30 +0900

    chore: Install packages and configure Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    vanillaExtractPlugin(),
+    libInjectCss(),
     dts({
       insertTypesEntry: true,
     }),
   ],
   build: {
+    copyPublicDir: false,
     lib: {
       entry: 'lib/index.ts',
       name: 'ReactVideoTimestone',
